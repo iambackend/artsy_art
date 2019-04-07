071805abe37e7d157ae2717f3dd1ae9b36106411
iambackend <iambackend@gmail.com>
2019-04-07 18:15:43 +0300

    multiprocessing, show progress, bugfixes, but not sure that this is for best

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageDraw, ImageEnhance
+from PIL import Image, ImageDraw
 from math import *
 from random import *
 
@@ -13,8 +13,7 @@ class Organism:
             for y in range(block_size):
                 orig = img.getpixel((x, y))
                 paro = parody.getpixel((x, y))
-                # print(orig, paro)
-                fatness += abs(orig[0] - paro[0])**2 + abs(orig[1] - paro[1])**2 + abs(orig[2] - paro[2])**2
+                fatness += (orig[0] - paro[0])**2 + (orig[1] - paro[1])**2 + (orig[2] - paro[2])**2
         return fatness
 
     def __init__(self, x, y, tilt, first_colour, second_colour):
@@ -27,9 +26,9 @@ class Organism:
                    (randrange(0, 255), randrange(0, 255), randrange(0, 255)))
 
     def mutate(self):
-        self.x += randrange(-2, 2)
-        self.y += randrange(-2, 2)
-        self.tilt += gauss(0, -0.3)
+        self.x = bound(self.x + randrange(-2, 2), -2, block_size + 2)
+        self.y = bound(self.y + randrange(-2, 2), -2, block_size + 2)
+        self.tilt = bound(self.tilt + gauss(0, -0.3), 0, 2*pi)
         self.first_colour = (bound(self.first_colour[0] + randrange(-10, 10), 0, 255),
                              bound(self.first_colour[1] + randrange(-10, 10), 0, 255),
                              bound(self.first_colour[2] + randrange(-10, 10), 0, 255))
@@ -50,17 +49,19 @@ class Organism:
                          int(prop * a.second_colour[2] + (1 - prop) * b.second_colour[2])))
 
     def paint(self):
-        # print(self.first_colour)
         img = Image.new('RGB', (block_size, block_size), color=self.first_colour)
         draw = ImageDraw.Draw(img)
-        if 0.5 * pi < self.tilt < 1.5 * pi:
-            draw.polygon([(-10000000, -10000000), (self.x - cos(self.tilt) * 10000, self.y - sin(self.tilt) * 10000),
+        if 0 < self.tilt < pi:
+            draw.polygon([(0, -1000000), (self.x - cos(self.tilt) * 10000, self.y - sin(self.tilt) * 10000),
                         (self.x + cos(self.tilt) * 10000, self.y + sin(self.tilt) * 10000)], fill=self.second_colour)
         else:
-            draw.polygon([(10000000, 10000000), (self.x - cos(self.tilt) * 10000, self.y - sin(self.tilt) * 10000),
+            draw.polygon([(0, 1000000), (self.x - cos(self.tilt) * 10000, self.y - sin(self.tilt) * 10000),
                           (self.x + cos(self.tilt) * 10000, self.y + sin(self.tilt) * 10000)], fill=self.second_colour)
-        # print(self.x, self.y, self.tilt)
         return img
+
+
+
+
 
 
 class Population:
@@ -70,17 +71,19 @@ class Population:
     def solve(self, size, generations):
         population = [Organism.random() for i in range(size)]
         sorted(population, key=lambda organism: organism.fatness(self.img))
-        # self.img.show()
+        stats = []
         for epoch in range(generations):
             for i in range(0, size // 3):
                 population[i + size // 3] = Organism.crossover(population[i], population[i + 1]).mutate()
             for i in range(size // 3 * 2, size):
                 population[i] = Organism.random() #Chernobyl trip
             population = sorted(population, key=lambda organism: organism.fatness(self.img))
-            # print([x.fatness(self.img) for x in population])
-            # if epoch % 10 == 0:
-            #     population[0].paint().show()
+            stats.append(population[0].fatness(self.img))
         return population[0].paint()
+
+
+
+
 
 import sys
 
@@ -89,9 +92,9 @@ if len(sys.argv) != 5:
           "python3 main.py image_path size_of_block population_size generations\n"
           "I recommend you following parameters:\n"
           "image_path: name of file in images directory, otherwise it will not work"
-          "size_of_block: 8/16/32, 1 will be just extremly slow convertion to bw\n"
-          "population: 10 is ok, never tried other values\n"
-          "generations: 10-30 is ok\n")
+          "size_of_block: 8/16/32\n"
+          "population: 30 is ok\n"
+          "generations: try it by yourself\n")
     sys.exit(-1)
 
 image_path = sys.argv[1]
@@ -99,31 +102,31 @@ block_size = int(sys.argv[2])
 population_size = int(sys.argv[3])
 generations = int(sys.argv[4])
 
+image_path = "avatar.jpg"
+
 im = Image.open("images/" + image_path).convert('RGB')
 (width, height) = im.size
-# enhancer = ImageEnhance.Color(im)
-# im = enhancer.enhance(200000)
-# enhancer = ImageEnhance.Contrast(im)
-# im = enhancer.enhance(10000)
-# enhancer = ImageEnhance.Brightness(im)
-# im = enhancer.enhance(0.1)
-im.show()
 
 etalon = [[im.crop((x, y, x + block_size, y + block_size)) for y in range(0, height, block_size)] for x in range(0, width, block_size)]
 
 result = Image.new('RGB', (width, height))
-# result.save("results/lenna_1_32.png", "PNG")
-for i in range(0, width // block_size):
-    for j in range(0, height // block_size):
-        pop = Population(etalon[i][j])
-        result.paste(pop.solve(population_size, generations), (i * block_size, j * block_size, (i + 1) * block_size, (j + 1) * block_size))
-    if i % (width // block_size // 8) == 0:
-        result.show()
+
+def solver(orig):
+    pop = Population(orig)
+    return pop.solve(population_size, generations)
+
+
+from multiprocessing import Pool
+with Pool(height // block_size) as p:
+    for i in range(0, width // block_size):
+        resultt = p.map(solver, etalon[i])
+        for j in range(0, height // block_size):
+            result.paste(resultt[j], (i * block_size, j * block_size, (i + 1) * block_size, (j + 1) * block_size))
+        print(str((i + 1) / (width // block_size) * 100) + '% is done')
+        # result.show()
 
 result.show()
-result.save("results/RGB_z" + str(block_size) + "_" + str(population_size) + "_" + str(generations) + "_" + image_path)
+result.save("results/RGB0.3_" + str(block_size) + "_" + str(population_size) + "_" + str(generations) + "_" + image_path)
 
-# for x in range(0, height // FRAME_SIZE):
-#     for y in range(0, width // FRAME_SIZE):
 
 
